46e53d30a5bbe875fc1973d1a585ea9d2a71b4d5
BeINone <sbeinone@gmail.com>
2018-04-01 23:57:15 +0900

    11654번: 아스키 코드

---
+++ baekjoon/string/11654.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * 11654번
+ * 아스키 코드
+ *
+ * https://www.acmicpc.net/problem/11654
+ */
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String letter = sc.next();
+        System.out.println((int) letter.charAt(0));
+    }
+}
