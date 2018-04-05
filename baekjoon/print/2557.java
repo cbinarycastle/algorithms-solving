f85b33252fe42d2af4940eb7131c3b3578e68858
BeINone <sbeinone@gmail.com>
2018-04-05 23:50:44 +0900

    2557번: Hello World

---
+++ baekjoon/print/2557.java
@@ -0,0 +1,12 @@
+/**
+ * 2557¹ø
+ * Hello World
+ * 
+ * https://www.acmicpc.net/problem/2557
+ */
+class Main {
+
+    public static void main(String[] args) {
+    	System.out.println("Hello World!");
+    }
+}
